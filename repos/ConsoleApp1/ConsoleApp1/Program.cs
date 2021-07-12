704fdcfed9ab098dfacb4581c6205af07f75464c
Navyashree G B <navyashreegb99@gmail.com>
2021-07-12 19:40:11 +0530

    Csharp started

---
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("Navya sffhgj ghjkl dfg");
+            if(x==9)
+            {
+                Console.WriteLine("Inside if");
+            }
+        }
+    }
+}
