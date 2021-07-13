0788c1b9fadb81fb75f9e8155af44ca58885e984
Navyashree G B <navyashreegb99@gmail.com>
2021-07-13 21:29:09 +0530

    Extra code added

--- repos/ConsoleApp1/ConsoleApp1/Program.cs
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿
 
+using System;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            Console.WriteLine("Navya sffhgj ghjkl dfg");
-            if(x==9)
+            for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine("Inside if");
+                Console.WriteLine("VALUE IS=" + args[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "user" && args[1] == "pass")
+            {
+                Console.WriteLine("welcome user");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("invalid  user");
+                Console.ReadLine();
             }
         }
+
     }
 }
---
+++ repos/forloop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    Console.WriteLine("i=" + i + " j=" + j);
+                }
+            }
+        }
+    }
+}
