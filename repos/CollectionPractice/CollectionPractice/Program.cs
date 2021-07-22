47b75b10ec7f1210520b5d1225d8e5fe918616b9
Navyashree G B <navyashreegb99@gmail.com>
2021-07-22 23:14:20 +0530

    collections

---
+++ repos/CollectionPractice/CollectionPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionPractice
+{
+    class Program
+    {
+        public void printArrayList(ArrayList al)
+        {
+            foreach (Object ob in al)
+            {
+                Console.Write(ob + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            ArrayList al = new ArrayList();
+            al.Add(100);
+            al.Add(200);
+            al.Add(300);
+            al.Add(200);
+            al.Add(600);
+            al.Add(700);
+            al.Insert(3, 400);
+            p.printArrayList(al);
+            al.Remove(200);
+            al.RemoveAt(3);
+
+            Console.WriteLine(al.Capacity);
+           p.printArrayList(al);
+        }
+
+        
+    }
+
+    class HashCollection
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("EId", 1);
+            ht.Add("Ename", "Navya");
+            ht.Add("Job", "ASE");
+            Console.WriteLine(ht["Enamew"]);
+
+            foreach(Object key in ht.Keys)
+            {
+                Console.WriteLine(key+": "+ht[key]);
+            }
+        }
+    }
+}
---
+++ repos/EnumTest/EnumTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EnumTest
+{
+    class Program
+    {
+        public enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+
+        public static void Main()
+        {
+            foreach (string s in Enum.GetNames(typeof(Days)))
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
