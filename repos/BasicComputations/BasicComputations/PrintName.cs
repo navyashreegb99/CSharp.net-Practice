466ad050756912a165630fa4028890daee5dfade
Navyashree G B <navyashreegb99@gmail.com>
2021-07-28 21:56:54 +0530

    Practice programs

---
+++ repos/BasicComputations/BasicComputations/PrintName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicComputations
+{
+    class PrintName
+    {
+       public static void Main()
+        {
+            Console.WriteLine("Enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of {0}+{1}={2}", num1, num2, num1 / num2);
+        }
+
+    }
+}
