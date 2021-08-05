334aaa066fac4f9917619d3886d8a4123f3d7f9c
Navyashree G B <navyashreegb99@gmail.com>
2021-08-05 17:49:31 +0530

    Copy constructors

--- repos/ConstructorPractice/ConstructorPractice/CopyConstructorEx.cs
+++ repos/ConstructorPractice/ConstructorPractice/CopyConstructorEx.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace ConstructorPractice
 {
-    class CopyConstructorEx
+   public class CopyConstructorEx
     {
+        String VehicleName;
+        String VehicleNo;
+        String State;
+
+       public CopyConstructorEx(String Vehiclename,String VehicleNo, String State)
+        {
+            this.VehicleName = Vehiclename;
+            this.VehicleNo = VehicleNo;
+            this.State = State;
+        }
+
+       public CopyConstructorEx(CopyConstructorEx vehicle)
+        {
+            VehicleName = vehicle.VehicleName;
+            VehicleNo = vehicle.VehicleNo;
+            State = vehicle.State;
+        }
+
+        public String Data
+        {
+            get
+            {
+                return "Vehicle name is: " + VehicleName + "Vehicle no is: " + VehicleNo + "It belongs to " + State;
+            }
+        }
+
+    }
+
+    class Testing
+    {
+        public static void Main()
+        {
+            CopyConstructorEx vehicle1 = new CopyConstructorEx("Maruthi Suzuki", "KA23DF08", "Karnataka");
+            CopyConstructorEx vehicle2 = new CopyConstructorEx(vehicle1);
+            Console.WriteLine(vehicle1.Data);
+            Console.WriteLine(vehicle2.Data);
+        }
     }
 }
---
+++ repos/ConstructorPractice/ConstructorPractice/StaticAndNonStaticConstructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorPractice
+{
+    class StaticAndNonStaticConstructor
+    {
+        int x;
+        static int y;
+
+        static StaticAndNonStaticConstructor()
+        {
+            Console.WriteLine("Static cons is called");
+        }
+
+       public StaticAndNonStaticConstructor()
+        {
+            Console.WriteLine("Non Static cons is called");
+        }
+
+        static void Main()
+        {
+            StaticAndNonStaticConstructor s1 = new StaticAndNonStaticConstructor();//Instance
+            StaticAndNonStaticConstructor s2=;//variable
+            StaticAndNonStaticConstructor s3 = s1;//reference
+        }
+    }
+}
